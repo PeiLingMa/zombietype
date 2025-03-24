b8509d5faca7a372bba1d50872c60015fc5cd2d7
Paris Shieh <parisdata@gmail.com>
2025-03-24 16:58:24 +0800

    feat(button): add back button to Story mode and Option page

--- src/components/StoryMode/StoryMode.js
+++ src/components/StoryMode/StoryMode.js
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 
-export default function StoryMode() {
-  return <h1>THIS IS STORYMODE PAGE</h1>;
+export default function StoryMode({ onBack }) {
+  return (
+    <div>
+      <h1>THIS IS STORYMODE PAGE</h1>
+      <>
+        <button
+          className="btn btn-info my-2 px-4 py-3 fs-4 fw-bold btn-lg mb-3"
+          onClick={onBack}
+        >
+          Back Menu
+        </button>
+      </>
+    </div>
+  );
 }
