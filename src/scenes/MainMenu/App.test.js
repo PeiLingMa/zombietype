4aaf9ef1f3fe6301f0f268ae88c7e09849881451
cjh5958 <jiaheng921122@gmail.com>
2025-04-16 13:39:53 +0800

    tests: fix import apth in  App.test.js

--- src/scenes/MainMenu/App.test.js
+++ src/scenes/MainMenu/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App from './scenes/MainMenu/App';
-import { GameSettingsProvider } from './context/GameSettingsContext';
+import App from './App';
+import { GameSettingsProvider } from '../../context/GameSettingsContext';
 
 test('renders main menu title', () => {
   render(
